f4f013df394d54e7ac054f723cac211c7f7ce653
Ayla Nelson <anelson@sonos.com>
2025-08-14 12:47:12 -0700

    script update TODO testing

--- vase/scripts/loadDb.ts
+++ vase/scripts/loadDb.ts
@@ -7,6 +7,9 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 import "dotenv/config" 
 
+//COMMENT: Type clearly defined
+type SimilarityMetric = "dot_product" | "cosine" | "euclidean"
+
 const { ASTRA_DB_NAMESPACE, 
     ASTRA_DB_COLLECTION, 
     ASTRA_DB_API_ENDPOINT, 
@@ -37,6 +40,57 @@ const splitter = new RecursiveCharacterTextSplitter({
     chunkOverlap: 100
 });
 
-const createCollection = async () => {
-    const res = await db.createCollection(ASTRA_DB_COLLECTION)
+const createCollection = async (similarityMetric: SimilarityMetric = "dot_product") => {
+    const res = await db.createCollection(ASTRA_DB_COLLECTION, {
+        vector: {
+            dimensions: 1536,
+            metric: similarityMetric,
+        }
+    })
+    console.log(res)
 }
+
+const loadSampleData = async () => {
+   const collection = await db.collection(ASTRA_DB_COLLECTION)
+   for await ( const url of vaseData){
+    const content = await scrapePage(url)
+    const chunks = await splitter.splitText(content);
+    for await (const chunk of chunks){
+        const embedding = await openai.embeddings.create({
+            model: "text-embedding-3-small",
+            input: chunk,
+            encoding_format: "float",
+        })
+
+        const vector = embedding.data[0].embedding
+
+        const res = await.collection.insertOne({
+            $vector: vector, 
+            text: chunk
+        })
+
+        console.log(res)
+
+    }
+   }
+}
+
+const scrapePage = async(url: string) => {
+    const loader = new PuppeteerWebBaseLoader(url, {
+        launchOptions: {
+            headless: true
+        },
+        gotoOptions: {
+            waitUntil: "domcontentloaded"
+        },
+        evaluate: async (page, browser) => {
+            const results = await page.evaluate(() => document.body.innerHTML)
+            await browser.close()
+            return result
+        }
+    })
+
+    return ( await loader.scrape())?.replace(/<[^>]*>?/gm, '')
+}
+
+createCollection().then(()=> loadSampleData())
